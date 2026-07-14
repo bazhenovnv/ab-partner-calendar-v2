a5ae19eac9167b58dc7118c18dc196b734a1e38a
bazhenovnv <n.bazhenov@a-b.ru>
2026-07-14 20:56:29 +0300

    fix(quotes): assemble approved quote frame asset

--- apps/frontend/src/components/quote-frame-data.ts
+++ apps/frontend/src/components/quote-frame-data.ts
@@ -1,2 +1,6 @@
-export const QUOTE_FRAME_DATA_URI =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA...';
+import part0 from './quote-frame-part-0';
+import part1 from './quote-frame-part-1';
+import part2 from './quote-frame-part-2';
+import part3 from './quote-frame-part-3';
+
+export const QUOTE_FRAME_DATA_URI = `data:image/png;base64,${part0}${part1}${part2}${part3}`;
