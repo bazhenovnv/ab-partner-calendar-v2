8d255ab7589f4f87761add76e3148553c1f0d5d4
bazhenovnv <n.bazhenov@a-b.ru>
2026-08-06 18:05:13 +0300

    feat(shared): экспортировать разбор города события

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './types/event';
 export * from './constants/index';
 export * from './constants/legal-documents';
 export * from './reminder-date-options';
+export * from './location-city';
