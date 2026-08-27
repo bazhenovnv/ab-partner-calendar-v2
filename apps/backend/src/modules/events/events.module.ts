49ca983e91dacb1c6e1163c0e0551e2f9af9d2be
bazhenovnv <n.bazhenov@a-b.ru>
2026-08-27 14:27:38 +0300

    fix: register publication location service

--- apps/backend/src/modules/events/events.module.ts
+++ apps/backend/src/modules/events/events.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { EventLifecycleService } from './event-lifecycle.service';
+import { EventPublicationLocationService } from './event-publication-location.service';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService, EventLifecycleService],
+  providers: [
+    EventsService,
+    EventLifecycleService,
+    EventPublicationLocationService,
+  ],
   exports: [EventsService],
 })
 export class EventsModule {}
