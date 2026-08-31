55094608fe510a61353259f71f4a86419ab73207
bazhenovnv <n.bazhenov@a-b.ru>
2026-08-31 09:31:34 +0300

    feat(editorial): add editorial publishing module

---
+++ apps/backend/src/modules/editorial/editorial.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { EditorialController } from './editorial.controller';
+import { EditorialService } from './editorial.service';
+
+@Module({
+  controllers: [EditorialController],
+  providers: [EditorialService],
+  exports: [EditorialService],
+})
+export class EditorialModule {}
